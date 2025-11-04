8c4a65c7438a44c34949af33a5b7bca87435024f
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-11-04 23:31:17 +0530

    alright it should work now

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+import os
 
+environment = os.environ.get('FLASK_ENV','dev')
 
-app = Flask(__name__)
+if environment=='production':
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html')
+
+elif environment == 'dev':
+    def create_app():
+        app = Flask(__name__)
+        @app.route('/')
+        def hello_world():
+            return render_template('index.html')
+
+        return app
+
+    if __name__ == '__main__':
+        app = create_app()
+        app.run(debug=True, host='0.0.0.0', port=8000)
