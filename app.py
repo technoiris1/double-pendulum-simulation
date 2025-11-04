a9fc1f264bf5978b071d14fcb951e97f524c3aa4
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-11-04 23:16:25 +0530

    im trying, ref
    ops is hell

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.config['DEVELOPMENT'] = os.environ.get('FLASK_ENV') == 'development'
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-    @app.route('/')
-    def hello_world():
-        return render_template('index.html')
-
-    return app
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=8000)
+@app.route('/about')
+def about():
+    return 'About'
