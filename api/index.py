de4bf5b3b4374d50b5b745cab2abe061a0051c18
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-11-04 22:55:32 +0530

    made a setup for deployment on vercel

---
+++ api/index.py
@@ -0,0 +1,5 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app import create_app
+app = create_app()
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+import os
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
 
+    app.config['DEVELOPMENT'] = os.environ.get('FLASK_ENV') == 'development'
+
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html')
+
+    return app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0', port=8000)
